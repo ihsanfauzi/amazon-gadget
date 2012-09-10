9f45a6ece397068bf156013df3e026bef706f141
AYuzafovich@gmail.com <AYuzafovich@gmail.com@5a7f36c4-fdd9-11de-b136-7f7962f7bc17>
2012-09-10 12:44:40 +0000

    Shipping price

--- trunk/amazon-check-shipping/src/amazon/check/shipping/BaseShippingPriceCalculator.java
+++ trunk/amazon-check-shipping/src/amazon/check/shipping/BaseShippingPriceCalculator.java
@@ -3,5 +3,19 @@ package amazon.check.shipping;
 import amazon.check.shipping.dto.ShippingPriceDTO;
 
 public abstract class BaseShippingPriceCalculator {
-	public abstract ShippingPriceDTO calculate(String region, String content);
+	public ShippingPriceDTO calculate(String region, String content) {
+		if (isSimple(content)) {
+			return calculateSimple(region, content);
+		} else {
+			return calculateComplex(region, content);
+		}
+	}
+	
+	public abstract ShippingPriceDTO calculateComplex(String region, String content);
+	
+	public abstract ShippingPriceDTO calculateSimple(String region, String content);
+	
+	public Boolean isSimple(String content) {
+		return content.indexOf("hideRates()") != -1;
+	}
 }
--- trunk/amazon-check-shipping/src/amazon/check/shipping/UkShippingPriceCalculator.java
+++ trunk/amazon-check-shipping/src/amazon/check/shipping/UkShippingPriceCalculator.java
@@ -11,13 +11,17 @@ public class UkShippingPriceCalculator extends BaseShippingPriceCalculator {
 	public static String REGION_North_America = "North America";
 	public static String REGION_Europe_Zone_2 = "Europe Zone 2";
 	public static String REGION_Japan = "Japan";
-	public static String REGION_UK_Mainland = "UK Mainland";
+	public static String REGION_UK_Street = "UK Street";
 	public static String REGION_UK_Mainland_PO_Box = "UK Mainland PO Box";
 	public static String REGION_BFPO = "BFPO";
-	
-	public ShippingPriceDTO calculate(String region, String content) {
+	@Override
+	public ShippingPriceDTO calculateComplex(String region, String content) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+	@Override
+	public ShippingPriceDTO calculateSimple(String region, String content) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
