57e150c00613a423d75df0bdd80eb67b0a787335
AYuzafovich <aaa@users.noreply.github.com>
2012-09-11 14:00:08 +0000

    Shipping rate

---
+++ amazon-check-shipping/src/amazon/check/shipping/dto/ShipmentBandsDTO.java
@@ -0,0 +1,7 @@
+package amazon.check.shipping.dto;
+
+public class ShipmentBandsDTO {
+	public Double from;
+	public Double to;
+	public Double price;
+}
--- amazon-check-shipping/test/test/ShippingPriceSimpleTests.java
+++ amazon-check-shipping/test/test/ShippingPriceSimpleTests.java
@@ -1,9 +1,5 @@
 package test;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-
 import org.junit.Before;
 import org.junit.Test;
 
@@ -17,7 +13,7 @@ import amazon.check.shipping.UsShippingPriceCalculator;
 import amazon.check.shipping.dto.ShippingPriceDTO;
 
 
-public class ShippingPriceTests {
+public class ShippingPriceSimpleTests extends ShippingPriceBaseTestCase {
 	@Before
 	public void init(){
 	}
@@ -207,26 +203,5 @@ public class ShippingPriceTests {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-	}
-	
-	private void traceSimple(String region, ShippingPriceDTO res) {
-		System.out.println("perItemExpedited " + res.perItemExpedited + " - " + region);
-		System.out.println("perItemStandard " + res.perItemStandard + " - " + region);
-		System.out.println("perShipmentExpedited " + res.perShipmentExpedited + " - " + region);
-		System.out.println("perShipmentStandard " + res.perShipmentStandard + " - " + region);
-		System.out.println("perWeightExpedited " + res.perWeightExpedited + " - " + region);
-		System.out.println("perWeightStandard " + res.perWeightStandard + " - " + region);
-		System.out.println();
-	}
-
-	private String loadContent(String string) throws Exception {
-		File f = new File(string);
-		String s = "";
-		String content = "";
-		BufferedReader br = new BufferedReader(new FileReader(f));
-		while((s = br.readLine())!=null) {
-			content = content + s + "\n";
-		}
-		return content;
 	}
 }
