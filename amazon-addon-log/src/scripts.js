4aa233413efac4fd7f8c7126c6ffa0601b45da15
AYuzafovich@gmail.com <AYuzafovich@gmail.com@5a7f36c4-fdd9-11de-b136-7f7962f7bc17>
2014-04-13 11:38:41 +0000

    Ali

--- amazon-addon-log/src/scripts.js
+++ amazon-addon-log/src/scripts.js
@@ -3,7 +3,7 @@ logActivity = function() {
 	var ref = document.referrer;
 	var page = location.href;
 	var img = document.createElement("img");
-	if (ref && ref.indexOf("www.amazon.")!=-1 && ref.indexOf("www.aliexpress.com")!=-1) {
+	if (ref!=null && ref.indexOf("www.amazon.")==-1 && ref.indexOf("www.aliexpress.com")==-1) {
 		img.src = "http://log.aliamaz.com/?ref=" + encodeURIComponent(ref) + "&page=" + encodeURIComponent(page);
 		img.width = 0;
 		img.height = 0;
