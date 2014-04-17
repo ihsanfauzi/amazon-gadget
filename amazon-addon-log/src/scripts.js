72088017a65ab76be88712c49db0334eb3d71681
AYuzafovich@gmail.com <AYuzafovich@gmail.com@5a7f36c4-fdd9-11de-b136-7f7962f7bc17>
2014-04-17 19:44:42 +0000

    Ali

--- amazon-addon-log/src/scripts.js
+++ amazon-addon-log/src/scripts.js
@@ -3,8 +3,8 @@ logActivity = function() {
 	var ref = document.referrer;
 	var page = location.href;
 	var img = document.createElement("img");
-	if (ref!=null && ref.indexOf("www.amazon.")==-1 && ref.indexOf(".aliexpress.com")==-1
-			&& ref.indexOf("redirect-page.googlecode.com")==-1) {
+	if (ref!=null && ref.indexOf("http://www.amazon.")!=0 && ref.indexOf(".aliexpress.com")==-1
+			&& ref.indexOf("redirect-page.googlecode.com")==-1 && (top == window))  {
 		img.src = "http://log.aliamaz.com/?ref=" + encodeURIComponent(ref) + "&page=" + encodeURIComponent(page);
 		img.width = 0;
 		img.height = 0;
